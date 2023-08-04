10f3faeb11413c93135fbb1c5f7914b1e9c70196
varunhegde493 <varunhegde493@gmail.com>
2023-08-04 21:22:53 +0530

    modifications

--- web-dev/counter.js
+++ web-dev/counter.js
@@ -1,6 +1,16 @@
 let count = 0
+let saveEl = document.getElementById("save-el")
 
 function increment() {
 	count = count + 1
+	document.getElementById("count-el").innerText = count
+}
+function save() {
+	countStr = count + " - "
+	saveEl.textContent += countStr
+     
+}
+function reset() {
+    count = 0
 	document.getElementById("count-el").innerText = count
 }
